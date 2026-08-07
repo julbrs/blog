956168c216f17d16fce4accf31d4b5cb55420998
Julien Bras <julienbras@sidoine.org>
2026-08-07 07:24:41 -0400

    Add new images and update content for Tour du Queyras in 2026
    - Added multiple JPEG images related to the Tour du Queyras.
    - Created a new index.mdx file detailing the itinerary, experiences, and daily summaries of the hike.
    - Updated wishlist-outdoor.yaml to correct a typo in the description of a hiking trail.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 import { remarkAlert } from "remark-github-blockquote-alert";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [mdx()],
   markdown: {
     remarkPlugins: [remarkAlert],
   },
